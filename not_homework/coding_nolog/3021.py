2283414abaa5054032947400214f9a77ef256718
icexer <icegammerrock@gmail.com>
2026-07-22 07:23:42 +0700

    test push

--- not_homework/coding_nolog/3021.py
+++ not_homework/coding_nolog/3021.py
@@ -6,3 +6,11 @@ r1 = int(input())
 x2 = int(input())
 y2 = int(input())
 r2 = int(input())
+
+# ห่างทั้งสอง
+dist = ((x1-x2) ** 2) + ((y1-y2) ** 2)
+radius = (r1 + r2) ** 2
+if dist <= radius:
+    print("overlapping")
+else:
+    print("no overlapping")
